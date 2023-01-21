462c3805aa19fb0b22b25a6ed9cf3c3d60d67223
krajka00 <jaguarkkii5@gmail.com>
2023-01-21 17:04:57 +0100

    add show client and delete client view

--- BazaDanychApp/Show_Clients.cs
+++ BazaDanychApp/Show_Clients.cs
@@ -11,28 +11,31 @@ using System.Windows.Forms;
 
 namespace BazaDanychApp
 {
-    public partial class Show_workers : Form
+    public partial class Show_Clients : Form
     {
-        public Show_workers()
+        public Show_Clients()
         {
             InitializeComponent();
         }
 
-        private void Back_Button_Click(object sender, EventArgs e)
+        private void Back_button_Click(object sender, EventArgs e)
         {
-            new Admin_Panel().Show();
+            new Client_Panel().Show();
             this.Close();
         }
 
-        private void Show_workers_Load(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Show_Clients_Load(object sender, EventArgs e)
         {
             Global.cnn.Open();
-            SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT imie, nazwisko, pesel, pracownik_id, ulica, nr_domu, nr_mieszkania, kod_pocztowy, nazwa_miasta FROM Dane_wszystkie WHERE pracownik_id IS NOT NULL", Global.cnn);
+            SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT imie, nazwisko, pesel, klient_id, ulica, nr_domu, nr_mieszkania, kod_pocztowy, nazwa_miasta FROM Dane_wszystkie WHERE klient_id IS NOT NULL", Global.cnn);
             DataTable dn = new DataTable();
             sqlDa.Fill(dn);
-
             dataGridView1.DataSource = dn;
-
             Global.cnn.Close();
         }
     }
